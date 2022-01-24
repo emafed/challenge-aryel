71cb33f13faa374f50c24ece242e45f996435356
emafed <manufederici72@gmail.com>
2022-01-24 23:56:52 +0100

    aggiunta dialog

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -7,12 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { UploadDialog } from './upload-dialog/upload-dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadDialog
   ],
   imports: [
     BrowserModule,
@@ -21,8 +24,9 @@ import {MatMenuModule} from '@angular/material/menu';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatListModule,
-    MatMenuModule
+    MatMenuModule,
+    MatTableModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
